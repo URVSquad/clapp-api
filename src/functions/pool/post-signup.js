fecd35e8d8e7244c2b06417a45a3291e6536c71c
Roger Bosch Mateo <boschmateo@MacBook-Pro-de-Roger.local>
2020-04-11 21:15:39 +0200

    post-signup

--- src/functions/pool/post-signup.js
+++ src/functions/pool/post-signup.js
@@ -2,13 +2,37 @@ const db = require('../../util/db');
 
 exports.postsignup = async (event, context, callback) => {
     let user_id = event.request.userAttributes.sub;
-    console.log(user_id);
-    let results = await db.transaction()
-        .query('INSERT INTO app_user (id) VALUES(?)', [user_id])
+    let is_enterprise = event.request.userAttributes.is_enterprise;
+    let name = event.request.userAttributes.name;
+    let email = event.request.userAttributes.email;
+    let username = event.request.userAttributes.username;
+
+    if (is_enterprise == 1){
+        is_enterprise = true;
+        let website = event.request.userAttributes.website;
+        let description = event.request.userAttributes.description;
+        let nif = event.request.userAttributes.nig;
+        console.log(user_id);
+        let results = await db.transaction()
+        .query('INSERT INTO app_user (id, name, website, nif, description, is_enterprise, email, username) VALUES(?, ?, ?, ?, ?, ?, ?, ?)', [user_id, name, website, nif, description, is_enterprise, email, username])
         .rollback(e => {
             return Error(e);
         }) // optional
         .commit(); // execute the queries
+    }
+    else{
+        is_enterprise = false;
+        console.log(user_id);
+        let results = await db.transaction()
+        .query('INSERT INTO app_user (id, name, is_enterprise, email, username) VALUES(?, ?, ?, ?, ?)', [user_id, name, is_enterprise, email, username])
+        .rollback(e => {
+            return Error(e);
+        }) // optional
+        .commit(); // execute the queries
+    }
+
+
+
 
     // Run clean up function
     await db.end();
