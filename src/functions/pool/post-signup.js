b62531a76ea9476c621c7b61e07aa67f87b4e8aa
Roger Bosch Mateo <boschmateo@MacBook-Pro-de-Roger.local>
2020-04-11 23:49:02 +0200

    Post-signup enterprise

--- src/functions/pool/post-signup.js
+++ src/functions/pool/post-signup.js
@@ -1,18 +1,21 @@
 const db = require('../../util/db');
 
 exports.postsignup = async (event, context, callback) => {
+    console.log(event.request.userAttributes);
+    //let body = JSON.parse(JSON.stringify(event.body));
+    //let ua = body.userAttributes;
+    //conole.log(ua);
     let user_id = event.request.userAttributes.sub;
-    let is_enterprise = event.request.userAttributes.is_enterprise;
+    let is_enterprise = event.request.userAttributes['custom:enterprise'];
     let name = event.request.userAttributes.name;
     let email = event.request.userAttributes.email;
     let username = event.request.userAttributes.username;
 
-    if (is_enterprise == 1){
+    if (is_enterprise == "1"){
         is_enterprise = true;
-        let website = event.request.userAttributes.website;
-        let description = event.request.userAttributes.description;
-        let nif = event.request.userAttributes.nig;
-        console.log(user_id);
+        let website = event.request.userAttributes['website'];
+        let description = event.request.userAttributes['custom:description'];
+        let nif = event.request.userAttributes['custom:nig'];
         let results = await db.transaction()
         .query('INSERT INTO app_user (id, name, website, nif, description, is_enterprise, email, username) VALUES(?, ?, ?, ?, ?, ?, ?, ?)', [user_id, name, website, nif, description, is_enterprise, email, username])
         .rollback(e => {
